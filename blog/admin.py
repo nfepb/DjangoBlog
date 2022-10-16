ee4322ea95a0404ee584c5ba9bd36c050f3f6a31
nfepb <nicolas.planteb@gmail.com>
2022-10-16 14:49:09 +0000

    Rearranges admin Postadmin features

--- blog/admin.py
+++ blog/admin.py
@@ -6,9 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 # Adds decorator to register Post model & PostAdmin class to site
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     # To add filtering panel based on selected fields:
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
-    list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title', 'content']
