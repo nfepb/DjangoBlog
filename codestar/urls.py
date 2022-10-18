bf0b6c3113cd45999b59819c7f1a9b78d08ec1ad
nfepb <nicolas.planteb@gmail.com>
2022-10-18 17:03:42 +0000

    Updates codestar>urls.py to include blog.urls path

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Register summernote url:
     path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls'), name='blog_urls'),
 ]
