722cd39d9e5994931bac12c675294d234caeb009
nfepb <nicolas.planteb@gmail.com>
2022-10-18 17:50:36 +0000

    Adds comments in blog/urls.py & updates <a> l.27 in templates/index.html to redirect to post_detail with the right slug

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,9 @@ from django.urls import path
 urlpatterns = [
     # Because using class based views, need to add "as_view"
     path('', views.PostList.as_view(), name='home'),
+    # 'slug' keyword matches 'slug' parameter in get method from
+    # PostDetail class in blog/views.py.
+    # This is how they are linked
+    #  1st slug is a path converter, 2nd is keyword name. 2nd could be anything
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
