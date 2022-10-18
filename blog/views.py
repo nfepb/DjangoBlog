011332ec7567bfd402ad38b6901cfd138a1f6c5e
nfepb <nicolas.planteb@gmail.com>
2022-10-18 17:21:15 +0000

    Updates blog>views.py with PostDetail class to display information about the specific post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 """
@@ -15,3 +15,29 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     # Limit number of Posts that can appear:
     paginate_by = 6
+
+
+class PostDetail(view):
+    # *args = other arguments, *kwargs = other key arguments
+    def get(self, request, slug, *args, **kwargs):
+        # Filter posts to only have active ones:
+        queryset = Post.objects.filter(status=1)
+        # Get published post with correct slug
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        # Check if user has liked the post
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exist():
+            liked = True
+
+        # Return all information with the 'render' method
+        return render(
+            request,
+            "post_detail.html",
+            # Dictionnary supplying context:
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            }
+        )
