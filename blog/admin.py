6936cf6f7e34ea6c268fb65d701bf41f4fa0a422
nfepb <nicolas.planteb@gmail.com>
2022-10-18 15:39:11 +0000

    Corrects typo in comment decorator in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ('title', 'content')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     # To add filtering panel based on selected fields:
     list_filter = ('status', 'created_on')
@@ -19,3 +19,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
