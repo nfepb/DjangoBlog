d860efd0815115a38c074462706e8862bb0812ac
nfepb <nicolas.planteb@gmail.com>
2022-10-19 16:06:39 +0000

    Adds Allauth package, updates requirements.txt, adds path to allauth in codestar/urls.py & adds the allauth to installed_apps in settings.py

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     # Register summernote url:
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
