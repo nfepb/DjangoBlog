12b0ff70a1bc74deb50d1a22eba24781319e7847
nfepb <nicolas.planteb@gmail.com>
2022-10-20 19:08:05 +0000

    Updates blog/views.py to handle post method for the comments on post, and display comments not approved in the get method

--- blog/views.py
+++ blog/views.py
@@ -39,6 +39,47 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
+                "liked": liked,
+                # Adding new key
+                "comment_form": CommentForm()
+            }
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        # Get all the data from the form and assign to variable
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment.form.instance.email = request.user.email
+            comment_form.instance.name = request.user.name
+            # Calling on the save method:
+            comment = comment_form.save(commit=False)
+            # Assign comment to a post
+            comment.post = post
+            # Commit save only after associating with post
+            comment.save()
+
+        else:
+            # If form is not valid, return empty form instance
+            comment_form = CommentForm()
+
+        # Return all information with the 'render' method
+        return render(
+            request,
+            "post_detail.html",
+            # Dictionnary supplying context:
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
                 "liked": liked,
                 # Adding new key
                 "comment_form": CommentForm()
